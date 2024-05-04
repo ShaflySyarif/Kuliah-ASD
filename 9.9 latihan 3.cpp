578d3fb14f4284527d3c9d21de8b7cdc1e7dbd56
ShaflySyarif <shaflysyarif1106@gmail.com>
2024-05-04 12:32:52 +0700

    Add files via upload

---
+++ 9.9 latihan 3.cpp
@@ -0,0 +1,72 @@
+//Latihan insertion sort (binary)
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#define MAX 10
+
+using namespace std;
+
+int Data[MAX];
+
+// Fungsi pengurutan penyisipan biner
+void BinaryInsertSort()
+{
+    int i, j, l, r, m, x;
+    int comparisons = 0;
+    int shifts = 0;
+
+    for (i = 1; i < MAX; i++) {
+        x = Data[i];
+        l = 0;
+        r = i - 1;
+
+        while (l <= r) {
+            m = (l + r) / 2;
+            comparisons++;
+
+            if (x > Data[m]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        for (j = i - 1; j >= l; j--) {
+            Data[j + 1] = Data[j];
+        }
+
+        Data[l] = x;
+        shifts++;
+        
+        cout << "Iterasi " << i << ": ";
+        for (j = 0; j < MAX; j++) {
+            cout << Data[j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Jumlah perbandingan: " << comparisons << endl;
+    cout << "Jumlah pergeseran: " << shifts << endl;
+}
+
+int main()
+{
+    int i;
+    srand(time(0));
+
+    // Membangkitkan bilangan acak
+    cout << "DATA SEBELUM TERURUT" << endl;
+    for (i = 0; i < MAX; i++) {
+        Data[i] = (int) rand() / 1000 + 1;
+        cout << "Data ke " << i << ": " << Data[i] << endl;
+    }
+
+    BinaryInsertSort();
+
+    // Data setelah terurut
+    cout << "DATA SETELAH TERURUT" << endl;
+    for (i = 0; i < MAX; i++) {
+        cout << "Data ke " << i << ": " << Data[i] << endl;
+    }
+}
